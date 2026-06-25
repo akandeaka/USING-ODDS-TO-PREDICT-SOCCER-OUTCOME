2e05497a47a66d405bed843b2c77c6ba4900442c
akandeaka <akande.akinpelu913@gmail.com>
2026-06-25 15:52:09 +0100

    Update 3_Performance_Log.py

--- portal/3_Performance_Log.py
+++ portal/3_Performance_Log.py
@@ -9,3 +9,4 @@ if not os.path.exists("performance_log.csv"):
 else:
     df = pd.read_csv("performance_log.csv")
     st.dataframe(df)
+    st.download_button("Download Log", df.to_csv(index=False), "performance_log.csv")
